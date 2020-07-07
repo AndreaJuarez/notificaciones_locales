f3966e7dd42db714d7d84c89119a14ded6fce533
AndreaJuarez <50895566+AndreaJuarez@users.noreply.github.com>
2020-07-06 23:38:25 -0500

    versión 1.0 funcionando

---
+++ lib/recordatorios_view.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'crud_operation.dart';
+import 'note.dart';
+
+class NoteList extends StatefulWidget {
+  @override
+  NoteListState createState() {
+    return new NoteListState();
+  }
+}
+
+class NoteListState extends State<NoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: FutureBuilder(
+        future: NoteProvider.getNoteList(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            final notes = snapshot.data;
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                Note(NoteMode.Editing, notes[index])));
+                  },
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 5.0, bottom: 5, left: 10.0, right: 22.0),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Row(children: <Widget>[
+                              new Expanded(
+                                  child: new Container(
+                                height: 100,
+                                decoration: new BoxDecoration(
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    color: Colors.blue),
+                                child: new Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    _NoteTitle(notes[index]['title']),
+                                    Container(
+                                      height: 4,
+                                    ),
+                                    _NoteText(notes[index]['text']),
+                                  ],
+                                ),
+                              )),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 2.0,
+                                  ),
+                              ),
+                              new Expanded(
+                                  child: new Container(
+                                height: 150,
+                                decoration: new BoxDecoration(
+                                    borderRadius:
+                                        new BorderRadius.circular(10.0),
+                                    color: Colors.blue),
+                                child: new Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    _NoteTitle(notes[index]['title']),
+                                    Container(
+                                      height: 4,
+                                    ),
+                                    _NoteText(notes[index]['text']),
+                                  ],
+                                ),
+                              )),
+                            ])
+                          ]),
+                    ),
+                  ),
+                );
+              },
+              itemCount: notes.length,
+            );
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Note(NoteMode.Adding, null)));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _NoteTitle extends StatelessWidget {
+  final String _title;
+
+  _NoteTitle(this._title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _title,
+      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    );
+  }
+}
+
+class _NoteText extends StatelessWidget {
+  final String _text;
+
+  _NoteText(this._text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _text,
+      style: TextStyle(color: Colors.grey.shade600),
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+}
